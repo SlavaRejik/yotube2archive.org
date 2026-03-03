8e12802702d58cb0e9c984f8d4e15cfbe6ab6d2b
Slava Rejik <slava@rejik.ru>
2026-03-03 10:21:23 +0300

    new sql

--- sys/oyvideo.sql
+++ sys/oyvideo.sql
@@ -8,6 +8,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 
+CREATE TABLE `channels` (
+  `id` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `ctime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE `oyids` (
   `oyid` varchar(255) NOT NULL,
   `ctime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
@@ -34,19 +41,27 @@ CREATE TABLE `playlists_members` (
 CREATE TABLE `videos` (
   `id` varchar(255) NOT NULL,
   `oyid` varchar(255) NOT NULL,
-  `place` enum('yotube','archive','vk') NOT NULL,
+  `place` enum('yotube','archive','vk','old.openyogaclass.com') NOT NULL,
+  `channel` varchar(255) DEFAULT NULL,
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `video_md5` varchar(255) NOT NULL,
   `lang` varchar(255) DEFAULT NULL,
   `license` varchar(255) DEFAULT NULL,
-  `subject-old` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `external-identifier-old` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   `storage` varchar(255) DEFAULT NULL,
-  `status` set('checked','downloaded') DEFAULT NULL,
+  `status` set('checked','downloaded','uploaded') DEFAULT NULL,
   `ctime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE `youtube_files_name` (
+  `channel_id` varchar(255) NOT NULL,
+  `video_id` varchar(255) NOT NULL,
+  `file_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+ALTER TABLE `channels`
+  ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `oyids`
   ADD PRIMARY KEY (`oyid`(20));
@@ -58,8 +73,11 @@ ALTER TABLE `playlists_members`
   ADD KEY `playlist_id` (`playlist_id`);
 
 ALTER TABLE `videos`
-  ADD PRIMARY KEY (`id`(16),`place`) USING BTREE,
-  ADD KEY `oyid` (`oyid`);
+  ADD KEY `oyid` (`oyid`),
+  ADD KEY `id` (`id`);
+
+ALTER TABLE `youtube_files_name`
+  ADD PRIMARY KEY (`video_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
